954b8db23a8610ff2744b672cec9b4c95e12e8c0
Waksmund <waksmund@o2.pl>
2026-01-31 19:42:16 +0100

    Edit button functional

--- gui.py
+++ gui.py
@@ -5,21 +5,41 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a to-do", key="todo") 
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos", 
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My To-Do App", 
-                    layout=[[label], [input_box, add_button]], 
+                    layout=[[label], [input_box, add_button], [list_box, edit_button]], 
                     font=("Helvetica", 20))
 
 while True:
     event, values = window.read() #returned by the window ('Add, {'todo', 'user input'})
-    print(event)
-    print(values)
+    print("Event: ", event)
+    print("Values: ", values)
+    if event == 'todos':
+        window['todo'].update(values['todos'][0].replace("\n", ""))
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todos = functions.get_todos() #pobranie listy
+            todo_to_edit =  values['todos'] #zadanie do edycji
+            # print(todos)
+            # print(str(todo_to_edit[0]))
+            ind = todos.index(todo_to_edit[0]) #index zadania do edycji
+            new_todo = values['todo'] + "\n"
+            print(todos)
+            # print(ind)
+            # print(todo_to_edit)
+            todos[ind] = new_todo #edycja zadania
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update("")
         case sg.WIN_CLOSED:
             break
 window.close()
